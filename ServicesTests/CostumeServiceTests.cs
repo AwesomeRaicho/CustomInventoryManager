12b58fe63eeceae97dd766673cafd0b8da0573bc
Ricardo Araujo <ricardo.dev0188@gmail.com>
2023-11-29 20:33:15 -0800

    finished mocked tests for CostumesService

--- ServicesTests/CostumeServiceTests.cs
+++ ServicesTests/CostumeServiceTests.cs
@@ -344,251 +344,266 @@ namespace ServicesTests
         }
         #endregion
 
-        //        #region GetFilteredCostumes
+        #region GetFilteredCostumes
+        [Fact]
+        public async Task GetFilteredCostumes_CorrectFiltering()
+        {
+            //Arrange
+            CostumeAddRequest costume1 = new CostumeAddRequest()
+            {
+                CostumeName = "Supaman",
+                Gender = GenderOptions.Male,
+                Age = "8",
+                Size = "10",
+                PurchasePrice = 150.35
+            };
+            CostumeAddRequest costume2 = new CostumeAddRequest()
+            {
+                CostumeName = "Batuman",
+                Gender = GenderOptions.Male,
+                Age = "13",
+                Size = "20",
+                PurchasePrice = 180.56
+            };
+            CostumeAddRequest costume3 = new CostumeAddRequest()
+            {
+                CostumeName = "Flash",
+                Gender = GenderOptions.Male,
+                Age = "4",
+                Size = "2",
+                PurchasePrice = 89.99
+            };
 
-        //        [Fact]
-        //        public void GetFilteredCostumes_CorrectFiltering()
-        //        {
-        //            //Arrange
-        //            CostumeAddRequest costume1 = new CostumeAddRequest()
-        //            {
-        //                CostumeName = "Supaman",
-        //                Gender = GenderOptions.Male,
-        //                Age = "8",
-        //                Size = "10",
-        //                PurchasePrice = 150.35
-        //            };
-        //            CostumeAddRequest costume2 = new CostumeAddRequest()
-        //            {
-        //                CostumeName = "Batuman",
-        //                Gender = GenderOptions.Male,
-        //                Age = "13",
-        //                Size = "20",
-        //                PurchasePrice = 180.56
-        //            };
-        //            CostumeAddRequest costume3 = new CostumeAddRequest()
-        //            {
-        //                CostumeName = "Flash",
-        //                Gender = GenderOptions.Male,
-        //                Age = "4",
-        //                Size = "2",
-        //                PurchasePrice = 89.99
-        //            };
+            List<CostumeResponse> costumeResponses_from_add = new List<CostumeResponse>();
+            List<Costume> repoReturn = new List<Costume>();
 
-        //            List<CostumeResponse> costumeResponses_from_add = new List<CostumeResponse>();
-        //            costumeResponses_from_add.Add(_costumeService.AddCostume(costume1));
-        //            costumeResponses_from_add.Add(_costumeService.AddCostume(costume2));
-        //            costumeResponses_from_add.Add(_costumeService.AddCostume(costume3));
+            costumeResponses_from_add.Add(costume1.ToCostume().ToCostumeResponse());
+            costumeResponses_from_add.Add(costume2.ToCostume().ToCostumeResponse());
+            costumeResponses_from_add.Add(costume3.ToCostume().ToCostumeResponse());
 
-
-
-        //            //getting all person names that match
-        //            List<CostumeResponse> filtered_costumeResponses_from_add = costumeResponses_from_add.Where(temp => (!string.IsNullOrEmpty(temp.CostumeName)) ? temp.CostumeName.Contains("man", StringComparison.OrdinalIgnoreCase) : false).ToList();
-        //            _testOutputHelper.WriteLine("\n*** Expected ***");
-        //            foreach(CostumeResponse costume in filtered_costumeResponses_from_add)
-        //            {
-        //                _testOutputHelper.WriteLine(costume.CostumeName);
-        //                _testOutputHelper.WriteLine(costume.CostumeID.ToString());
-        //            }
-        //            //Act
-        //            List<CostumeResponse> costumeResponses_from_getFiltered = _costumeService.GetFilteredCostumes(nameof(Costume.CostumeName), "man");
-
-        //            _testOutputHelper.WriteLine("\n*** Actual ***");
-        //            foreach (CostumeResponse costume in costumeResponses_from_getFiltered)
-        //            {
-        //                _testOutputHelper.WriteLine(costume.CostumeName);
-        //                _testOutputHelper.WriteLine(costume.CostumeID.ToString());
-        //            }
+            repoReturn.Add(costume1.ToCostume());
+            repoReturn.Add(costume2.ToCostume());
+            repoReturn.Add(costume3.ToCostume());
 
 
-        //            //Assert
 
-        //            //check that 'Flash' is not in the list
-        //            Assert.DoesNotContain(costumeResponses_from_add[2], costumeResponses_from_getFiltered);
+            //getting all person names that match
+            List<CostumeResponse> filtered_costumeResponses_from_add = costumeResponses_from_add.Where(temp => (!string.IsNullOrEmpty(temp.CostumeName)) ? temp.CostumeName.Contains("man", StringComparison.OrdinalIgnoreCase) : false).ToList();
 
-        //            //checkes that names containing 'man' are on the list
-        //            foreach(CostumeResponse costume in filtered_costumeResponses_from_add)
-        //            {
-        //                Assert.Contains(costume, costumeResponses_from_getFiltered);
-        //            }
-        //        }
-        //        #endregion
+            _testOutputHelper.WriteLine("\n*** Expected ***");
 
-        //        #region DeleteCostume
+            foreach (CostumeResponse costume in filtered_costumeResponses_from_add)
+            {
+                _testOutputHelper.WriteLine(costume.CostumeName);
+                _testOutputHelper.WriteLine(costume.CostumeID.ToString());
+            }
 
-        //        //Null ID param should return exception
-        //        [Fact]
-        //        public void DeleteCostume_NullID()
-        //        {
-        //            //Arrange
-        //            Guid? id = null;
+            _costumeRepo.Setup(x => x.GetAll(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(repoReturn);
 
+            //Act
+            List<CostumeResponse> costumeResponses_from_getFiltered = await _costumeService.GetFilteredCostumes(nameof(Costume.CostumeName), "man");
 
-        //            Assert.Throws<ArgumentNullException>(() =>
-        //            {
-        //                //Act
-        //                bool response = _costumeService.DeleteCostume(id);
-
-        //            });
-        //        }
+            _testOutputHelper.WriteLine("\n*** Actual ***");
+            foreach (CostumeResponse costume in costumeResponses_from_getFiltered)
+            {
+                _testOutputHelper.WriteLine(costume.CostumeName);
+                _testOutputHelper.WriteLine(costume.CostumeID.ToString());
+            }
 
 
-        //        // Provided ID does not exist in the DB, should return false
-        //        [Fact]
-        //        public void DeleteCostume_IDNotInDB()
-        //        {
-        //            //Arrange
-        //            Guid? id = Guid.NewGuid();
+            //Assert
 
-        //            //Act
-        //            bool response = _costumeService.DeleteCostume(id);
+            //check that 'Flash' is not in the list
+            Assert.DoesNotContain(costumeResponses_from_add[2], costumeResponses_from_getFiltered);
 
-        //            //Assert
+            //checkes that names containing 'man' are on the list
+            foreach (CostumeResponse costume in filtered_costumeResponses_from_add)
+            {
+                Assert.Contains(costume, costumeResponses_from_getFiltered);
+            }
+        }
+        #endregion
 
-        //            Assert.False(response);
-        //        }
+        #region DeleteCostume
 
-
-        //        //Correctly delete entry from DB
-        //        [Fact]
-        //        public void DeleteCostume_DeletedCorrectly()
-        //        {
-        //            //Arrange
-        //            CostumeAddRequest costume_request = new CostumeAddRequest()
-        //            {
-        //                CostumeName = "supeman",
-        //                Gender = GenderOptions.Male,
-        //                Age = "10",
-        //                Size = "10",
-        //                PurchasePrice = 125.35
-        //            };
-
-        //            CostumeResponse costume_response = _costumeService.AddCostume(costume_request);
-
-        //            //Act
-        //            bool response = _costumeService.DeleteCostume(costume_response.CostumeID);
-
-        //            //Assert
-
-        //            Assert.True(response);
-        //        }
-        //        #endregion
-
-        //        #region GetSortedCostumes
-
-        //        [Fact]
-        //        public void GetSortedCostumes_CorrectlySorted()
-        //        {
-        //            //Arrange
-        //            CostumeAddRequest costume0 = new CostumeAddRequest()
-        //            {
-        //                CostumeName = "Wondawoman",
-        //                Gender = GenderOptions.Female,
-        //                Age = "8",
-        //                Size = "10",
-        //                PurchasePrice = 333.33
-        //            };
-        //            CostumeAddRequest costume2 = new CostumeAddRequest()
-        //            {
-        //                CostumeName = "Batuman",
-        //                Gender = GenderOptions.Male,
-        //                Age = "13",
-        //                Size = "20",
-        //                PurchasePrice = 180.56
-        //            };
-        //            CostumeAddRequest costume1 = new CostumeAddRequest()
-        //            {
-        //                CostumeName = "Supaman",
-        //                Gender = GenderOptions.Male,
-        //                Age = "8",
-        //                Size = "10",
-        //                PurchasePrice = 150.35
-        //            };
-        //            CostumeAddRequest costume3 = new CostumeAddRequest()
-        //            {
-        //                CostumeName = "Flash",
-        //                Gender = GenderOptions.Male,
-        //                Age = "4",
-        //                Size = "2",
-        //                PurchasePrice = 89.99
-        //            };
-
-        //            List<CostumeResponse> costumeResponses_from_add = new List<CostumeResponse>();
-        //            costumeResponses_from_add.Add(_costumeService.AddCostume(costume0));
-        //            costumeResponses_from_add.Add(_costumeService.AddCostume(costume1));
-        //            costumeResponses_from_add.Add(_costumeService.AddCostume(costume2));
-        //            costumeResponses_from_add.Add(_costumeService.AddCostume(costume3));
-
-        //            List<CostumeResponse> costumeResponses_from_add_sorted = costumeResponses_from_add.OrderBy(temp => temp.CostumeName).ToList();
-
-        //            //Act
-        //            List<CostumeResponse> costumeResponses_from_getSort = _costumeService.GetSortedCostumes(costumeResponses_from_add, nameof(Costume.CostumeName), SortOrderOptions.ASC);
-
-        //            //Assert
-        //            for(int i = 0; i<costumeResponses_from_add.Count; i++)
-        //            {
-        //                Assert.Equal(costumeResponses_from_add_sorted[i], costumeResponses_from_getSort[i]);
-        //            }
-
-        //        }
-
-        //        #endregion
-
-        //        #region UpdateCostume
-
-        //        [Fact]
-        //        public void UpdateCostume_NullCostume()
-        //        {
-        //            //Arrange
-        //            CostumeUpdateRequest? costume_to_update = null;
-
-        //            //Assert
-        //            Assert.Throws<ArgumentNullException>(() =>
-        //            {
-        //                //Act
-        //                _costumeService.UpdateCostume(costume_to_update);
-        //            });
-        //        }
+        //Null ID param should return exception
+        [Fact]
+        public async Task DeleteCostume_NullID()
+        {
+            //Arrange
+            Guid? id = null;
 
 
-        //        [Fact]
-        //        public void UpdateCostume_ProperUpdate()
-        //        {
-        //            //Arrange
-        //            CostumeAddRequest costume = new CostumeAddRequest()
-        //            {
-        //                CostumeName = "Flash",
-        //                Gender = GenderOptions.Male,
-        //                Age = "4",
-        //                Size = "2",
-        //                PurchasePrice = 89.99
-        //            };
+            await Assert.ThrowsAsync<ArgumentNullException>( async () =>
+            {
+                //Act
+                bool response = await _costumeService.DeleteCostume(id);
 
-        //            CostumeResponse costume_response = _costumeService.AddCostume(costume);
-
-        //            CostumeUpdateRequest costume_update = costume_response.GetCostumeUpdateRequest();
-        //            costume_update.CostumeName = "ReverseFlash";
-
-        //            //Act
-        //            _costumeService.UpdateCostume(costume_update);
-
-        //            //get costume from DB and verify if the new name is on it
-        //            CostumeResponse? costume_from_get = _costumeService.GetCostumeByCostumeID(costume_response.CostumeID);
-
-        //            // Assert
-        //            if (costume_from_get != null)
-        //            {
-        //                Assert.Equal("ReverseFlash", costume_from_get.CostumeName);
-        //            }
-        //            else
-        //            {
-        //                // Handle the case where the costume is null, for example:
-        //                Assert.True(false, "The costume returned by _costumeService.GetCostumeByCostumeID is null.");
-        //            }
-        //        }
+            });
+        }
 
 
-        //        #endregion
+        // Provided ID does not exist in the DB, should return false
+        [Fact]
+        public async Task DeleteCostume_IDNotInDB()
+        {
+            //Arrange
+            Guid? id = Guid.NewGuid();
+
+            //Act
+            bool response = await _costumeService.DeleteCostume(id);
+
+            //Assert
+
+            Assert.False(response);
+        }
+
+
+        //Correctly delete entry from DB
+        [Fact]
+        public async Task DeleteCostume_DeletedCorrectly()
+        {
+            //Arrange
+            CostumeAddRequest costume_request = new CostumeAddRequest()
+            {
+                CostumeName = "supeman",
+                Gender = GenderOptions.Male,
+                Age = "10",
+                Size = "10",
+                PurchasePrice = 125.35
+            };
+
+            CostumeResponse costume_response = costume_request.ToCostume().ToCostumeResponse();
+
+            _costumeRepo.Setup(x => x.GetById(It.IsAny<Guid>())).ReturnsAsync(costume_request.ToCostume());
+
+            //Act
+            bool response = await _costumeService.DeleteCostume(costume_response.CostumeID);
+
+            //Assert
+
+            Assert.True(response);
+        }
+        #endregion
+
+        #region GetSortedCostumes
+
+        [Fact]
+        public async Task GetSortedCostumes_CorrectlySorted()
+        {
+            //Arrange
+            CostumeAddRequest costume0 = new CostumeAddRequest()
+            {
+                CostumeName = "Wondawoman",
+                Gender = GenderOptions.Female,
+                Age = "8",
+                Size = "10",
+                PurchasePrice = 333.33
+            };
+            CostumeAddRequest costume2 = new CostumeAddRequest()
+            {
+                CostumeName = "Batuman",
+                Gender = GenderOptions.Male,
+                Age = "13",
+                Size = "20",
+                PurchasePrice = 180.56
+            };
+            CostumeAddRequest costume1 = new CostumeAddRequest()
+            {
+                CostumeName = "Supaman",
+                Gender = GenderOptions.Male,
+                Age = "8",
+                Size = "10",
+                PurchasePrice = 150.35
+            };
+            CostumeAddRequest costume3 = new CostumeAddRequest()
+            {
+                CostumeName = "Flash",
+                Gender = GenderOptions.Male,
+                Age = "4",
+                Size = "2",
+                PurchasePrice = 89.99
+            };
+
+            List<CostumeResponse> costumeResponses_from_add = new List<CostumeResponse>();
+            costumeResponses_from_add.Add(costume0.ToCostume().ToCostumeResponse());
+            costumeResponses_from_add.Add(costume1.ToCostume().ToCostumeResponse());
+            costumeResponses_from_add.Add(costume2.ToCostume().ToCostumeResponse());
+            costumeResponses_from_add.Add(costume3.ToCostume().ToCostumeResponse());
+
+
+            List<CostumeResponse> costumeResponses_from_add_sorted = costumeResponses_from_add.OrderBy(temp => temp.CostumeName).ToList();
+
+            //Act
+            List<CostumeResponse> costumeResponses_from_getSort = await _costumeService.GetSortedCostumes(costumeResponses_from_add, nameof(Costume.CostumeName), SortOrderOptions.ASC);
+
+            //Assert
+            for (int i = 0; i < costumeResponses_from_add.Count; i++)
+            {
+                Assert.Equal(costumeResponses_from_add_sorted[i], costumeResponses_from_getSort[i]);
+            }
+
+        }
+
+        #endregion
+
+        #region UpdateCostume
+
+        [Fact]
+        public async Task UpdateCostume_NullCostume()
+        {
+            //Arrange
+            CostumeUpdateRequest? costume_to_update = null;
+
+            //Assert
+            await Assert.ThrowsAsync<ArgumentNullException>( async () =>
+            {
+                //Act
+                await _costumeService.UpdateCostume(costume_to_update);
+            });
+        }
+
+
+        [Fact]
+        public async Task UpdateCostume_ProperUpdate()
+        {
+            //Arrange
+            CostumeAddRequest costume = new CostumeAddRequest()
+            {
+                CostumeName = "Flash",
+                Gender = GenderOptions.Male,
+                Age = "4",
+                Size = "2",
+                PurchasePrice = 89.99
+            };
+
+            CostumeResponse costume_response = costume.ToCostume().ToCostumeResponse();
+
+            CostumeUpdateRequest costume_update = costume_response.GetCostumeUpdateRequest();
+            costume_update.CostumeName = "ReverseFlash";
+
+            _costumeRepo.Setup(x => x.GetById(It.IsAny<Guid>())).ReturnsAsync(costume.ToCostume());
+
+            //Act
+            await _costumeService.UpdateCostume(costume_update);
+
+            //get costume from DB and verify if the new name is on it
+            CostumeResponse? costume_from_get = await _costumeService.GetCostumeByCostumeID(costume_response.CostumeID);
+
+            // Assert
+            if (costume_from_get != null)
+            {
+                Assert.Equal("ReverseFlash", costume_from_get.CostumeName);
+            }
+            else
+            {
+                // Handle the case where the costume is null, for example:
+                Assert.True(false, "The costume returned by _costumeService.GetCostumeByCostumeID is null.");
+            }
+        }
+
+
+        #endregion
 
     }
 }
